c237366adfc971bfbef93ebd360838ad007c9fbb
Yu Cao <tsaoyu@tsaoyu.com>
2020-03-21 16:17:20 +0000

    Network settings

--- redrov/src/redrov_missions/scripts/laodi_setup.bash
+++ redrov/src/redrov_missions/scripts/laodi_setup.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export ROS_IP=192.168.2.2
-export ROS_MASTER_URI=http://192.168.2.2:11311
+echo export ROS_MASTER_URI=http://192.168.2.2:11311
+echo export ROS_HOSTNAME=192.168.2.2
 source /home/laodi/ORCA_control/redrov/devel/setup.bash
 exec "$@"
--- redrov/src/redrov_missions/scripts/yu_setup.bash
+++ redrov/src/redrov_missions/scripts/yu_setup.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-export ROS_IP=192.168.2.2
-export ROS_MASTER_URI=http://192.168.2.2:11311
+# export ROS_IP=192.168.2.1
+echo export ROS_MASTER_URI=http://192.168.2.2:11311
+echo export ROS_HOSTNAME=192.168.2.1
 source /home/yu/ORCA_control/redrov/devel/setup.bash
 exec "$@"
