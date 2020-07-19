5fca388af6fbb2f8af237cd5b4ca8eb0f8ff2523
Yu Cao <tsaoyu@tsaoyu.com>
2020-07-19 16:56:27 +0100

    Control multiplexer

---
+++ redrov/src/redrov_control/src/redrov_control/rqt_module.py
@@ -0,0 +1,90 @@
+import os
+import rospkg
+import rospy
+
+from std_msgs.msg import String
+
+from qt_gui.plugin import Plugin
+from python_qt_binding import loadUi
+from python_qt_binding.QtWidgets import QWidget
+
+from redrov_control.QLed import QLed
+
+class MyPlugin(Plugin):
+
+    def __init__(self, context):
+        super(MyPlugin, self).__init__(context)
+        # Give QObjects reasonable names
+        self.setObjectName('MyPlugin')
+        rp = rospkg.RosPack()
+
+        # Process standalone plugin command-line arguments
+        from argparse import ArgumentParser
+        parser = ArgumentParser()
+        # Add argument(s) to the parser.
+        parser.add_argument("-q", "--quiet", action="store_true",
+                      dest="quiet",
+                      help="Put plugin in silent mode")
+        args, unknowns = parser.parse_known_args(context.argv())
+        if not args.quiet:
+            print 'arguments: ', args
+            print 'unknowns: ', unknowns
+
+        # Create QWidget
+        self._widget = QWidget()
+        # Get path to UI file which is a sibling of this file
+        # in this example the .ui and .py file are in the same folder
+        ui_file = os.path.join(rp.get_path('redrov_control'), 'resource', 'mux.ui')
+        # Extend the widget with all attributes and children from UI file
+        loadUi(ui_file, self._widget)
+        # Give QObjects reasonable names
+        self._widget.setObjectName('MyPluginUi')
+        # Show _widget.windowTitle on left-top of each plugin (when 
+        # it's set in _widget). This is useful when you open multiple 
+        # plugins at once. Also if you open multiple instances of your 
+        # plugin at once, these lines add number to make it easy to 
+        # tell from pane to pane.
+        if context.serial_number() > 1:
+            self._widget.setWindowTitle(self._widget.windowTitle() + (' (%d)' % context.serial_number()))
+        # Add widget to the user interface
+        context.add_widget(self._widget)
+
+        self.pub = rospy.Publisher('/use_controller', String, queue_size=10)
+        self._widget.pushButton.clicked.connect(self.pid_botton_callback)
+        self._widget.pushButton_2.clicked.connect(self.lqr_botton_callback)
+        self._widget.pushButton_3.clicked.connect(self.mpc_botton_callback)
+
+    def shutdown_plugin(self):
+        # TODO unregister all publishers here
+        self.pub.unregister()
+        pass
+
+    def save_settings(self, plugin_settings, instance_settings):
+        # TODO save intrinsic configuration, usually using:
+        # instance_settings.set_value(k, v)
+        pass
+
+    def restore_settings(self, plugin_settings, instance_settings):
+        # TODO restore intrinsic configuration, usually using:
+        # v = instance_settings.value(k)
+        pass
+
+    #def trigger_configuration(self):
+        # Comment in to signal that the plugin has a way to configure
+        # This will enable a setting button (gear icon) in each dock widget title bar
+        # Usually used to open a modal configuration dialog
+
+    def pid_botton_callback(self):
+        control = String()
+        control.data = 'pid'
+        self.pub.publish(control)
+
+    def lqr_botton_callback(self):
+        control = String()
+        control.data = 'lqr'
+        self.pub.publish(control)
+
+    def mpc_botton_callback(self):
+        control = String()
+        control.data = 'mpc'
+        self.pub.publish(control)
