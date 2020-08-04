b43220d811df33daa428b8b3c46ff5a94b03978b
Yu Cao <tsaoyu@tsaoyu.com>
2020-08-04 10:58:31 +0100

    Update package

--- utilities/pullfrompi.bash
+++ utilities/pullfrompi.bash
@@ -2,7 +2,7 @@
 set -e
 
 echo "On ${BLUEROV2_IP:=192.168.2.2}, pushing to bareclone..."
-ssh ${JETSON_USER:=laodi}@$BLUEROV2_IP 'cd ~/ORCA_control; git push bareclone master'
+ssh ${JETSON_USER:=pi}@$BLUEROV2_IP 'cd ~/ORCA_control; git push bareclone master'
 
 echo "Pulling from the pi..."
 git pull $JETSON_USER@$BLUEROV2_IP:orca-bluerov-bare master
