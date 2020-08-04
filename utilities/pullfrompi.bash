d62d734e2925799756e14f986815cb7f6a10c289
Yu Cao <tsaoyu@tsaoyu.com>
2020-08-04 12:19:50 +0100

    Pull for HW robot

--- utilities/pullfrompi.bash
+++ utilities/pullfrompi.bash
@@ -2,7 +2,7 @@
 set -e
 
 echo "On ${BLUEROV2_IP:=192.168.2.2}, pushing to bareclone..."
-ssh ${JETSON_USER:=pi}@$BLUEROV2_IP 'cd ~/ORCA_control; git push bareclone master'
+ssh ${JETSON_USER:=pi}@$BLUEROV2_IP 'cd ~/ORCA_control; git push bareclone heavy-dev'
 
 echo "Pulling from the pi..."
-git pull $JETSON_USER@$BLUEROV2_IP:orca-bluerov-bare master
+git pull $JETSON_USER@$BLUEROV2_IP:orca-bluerov-bare heavy-dev
